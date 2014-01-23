f28abd88eb28edc91793d03c56572b30f905d6e2
Tom Demeranville <tom@demeranville.com>
2014-01-23 21:24:05 +0000

    doc update

--- src/main/java/uk/bl/odin/orcid/client/OrcidConstants.java
+++ src/main/java/uk/bl/odin/orcid/client/OrcidConstants.java
@@ -4,7 +4,6 @@ import org.restlet.data.MediaType;
 
 /**
  * ORCID message constants not defined in schema
- * TODO: make enumerations from work-type and identifier-type
  */
 public class OrcidConstants {
 	
@@ -15,9 +14,12 @@ public class OrcidConstants {
 	public static final MediaType APPLICATION_ORCID_XML = MediaType.register("application/orcid+xml",
 			"application/orcid+xml");
 	
+	//TODO: enumerate search param types
 	public static final String SEARCH_PARAM_DOI = "digital-object-ids";
 	public static String MESSAGE_VERSION = "1.1";
 	
+	//TODO: enumerate roles
 	public static String ROLE_AUTHOR = "author";
+	//TODO: emumerate sequences
 	public static String SEQUENCE_FIRST = "first";
 }
