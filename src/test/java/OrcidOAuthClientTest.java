1da5caf680e961c48b63ca34ad0bf3a879e08095
Tom Demeranville <tom@demeranville.com>
2014-07-11 09:55:12 +0100

    fixed sandbox url, added method to fetch create profile token and unit test for it

---
+++ src/test/java/OrcidOAuthClientTest.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.bl.odin.orcid.client.OrcidAccessToken;
+import uk.bl.odin.orcid.client.OrcidOAuthClient;
+import uk.bl.odin.orcid.client.constants.OrcidAuthScope;
+
+
+
+/** Most tests migrated to orcid-update-app as it requires server interaction.
+ * 
+ * Developers should ensure that they copy testoauth.properties.example to testoauth.properties
+ * and modify it to contain their credentials.
+ *  
+ * @author tom
+ *
+ */
+
+public class OrcidOAuthClientTest {
+	private Properties properties = new Properties();
+
+	@Before
+	public void before() throws IOException {
+		properties.load(getClass().getResourceAsStream("testoauth.properties"));
+	}
+	
+	@Test
+	public final void testCreateProfile() throws IOException, JAXBException {
+		OrcidOAuthClient client = new OrcidOAuthClient(properties.getProperty("orcidClientID"),
+				properties.getProperty("orcidClientSecret"), properties.getProperty("orcidReturnUri"),
+				Boolean.valueOf(properties.getProperty("orcidSandbox")));
+		OrcidAccessToken tok = client.getCreateProfileAccessToken();
+		System.out.print(tok.getAccess_token());
+	}
+	
+}
